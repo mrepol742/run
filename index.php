8f02e2b87f002a62ba2a79dd4f47e78c3127e4d5
Melvin Jones Repol <mrepol742@gmail.com>
2023-03-04 08:11:39 +0000

    Initial Commit

--- index.php
+++ index.php
@@ -3,17 +3,62 @@ $code = $_POST["Code"] ?? "";
 $lang = $_POST["Lang"] ?? "";
 
 if ($code == "") {
-    exit("{\"error\":\"742\",\"description\":\"?code (Code) is not defined.\"}");
+    exit("{\"error\":\"742\",\"description\":\" (Code) is not defined.\"}");
 }
 if ($lang == "") {
-    exit("{\"error\":\"743\",\"description\":\"&lang (Language) is not defined.\"}");
+    exit("{\"error\":\"743\",\"description\":\" (Language) is not defined.\"}");
 }
 
-$filen = getTimestamp() . ".php";
-write($filen, $code);
-$output = run("php " . $filen);
-echo $output;
-unlink($filen);
+switch ($lang) {
+    case "php":
+        $filen = getTimestamp() . ".php";
+        write($filen, $code);
+        $output = run("php " . $filen);
+        echo $output;
+        unlink($filen);
+    break;
+    case "etc":
+        $output = run($code);
+        echo $output;
+    break;
+  case "c":
+   $filen = getTimestamp() . ".c";
+        write($filen, $code);
+         run("gcc " . $filen);
+   $output = run("./a.out");
+        echo $output;
+        unlink($filen);
+  break;
+  case "c++":
+  case "cpp":
+   case "cplusplus":
+   $filen = getTimestamp() . ".cpp";
+        write($filen, $code);
+         run("g++ " . $filen);
+   $output = run("./a.out");
+        echo $output;
+        unlink($filen);
+  break;
+  case "js":
+  case "javascript":
+   $filen = getTimestamp() . ".js";
+        write($filen, $code);
+        $output = run("node " . $filen);
+        echo $output;
+        unlink($filen);
+  break;
+    case "java":
+        $filen = getTimestamp();
+        write($filen . ".java", $code);
+      run("javac " . $filen . ".java");
+        $output = run("java Main");
+        echo $output;
+        unlink($filen . ".java");
+    break;
+    default:
+    exit("{\"error\":\"744\",\"description\":\"Language is not supported.\"}");
+    break;
+}
 
 function write($file, $content) {
     $myfile = fopen($file, "w") or die("{\"error\":\"769\",\"description\":\"Unable to write file.\"}");
@@ -29,6 +74,6 @@ function getTimestamp() {
 }
 
 function run($cmd) {
-    return shell_exec($cmd);
+    return shell_exec($cmd . " 2>&1");
 }
 ?>
--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.php82
+		pkgs.nodejs-16_x
+  pkgs.adoptopenjdk-openj9-bin-8
+  pkgs.jre_minimal
+  pkgs.php82
 	];
 }
